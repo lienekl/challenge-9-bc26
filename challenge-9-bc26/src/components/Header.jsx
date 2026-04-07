1f4c705d35b779d03094fbb43646c8cb1cc4d2b4
lienekl <lienekl@yahoo.com>
2026-04-07 21:37:34 +0100

    Added components and pages

---
+++ challenge-9-bc26/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h1>Liene Klavina</h1>
+            </div>
+            <div>
+                <img src="./assets/profile-picture.png" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
